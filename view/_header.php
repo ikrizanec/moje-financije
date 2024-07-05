3e1c57735a6e559d26b815e4f71527ceb6d426ae
Ivana Križanec <ivanakrizanec@hotmail.com>
2024-07-05 17:33:17 +0200

    Ispravljene rute za odabir u navbaru.

--- view/_header.php
+++ view/_header.php
@@ -10,12 +10,12 @@
 </head>
 <body>
     <div class="topnav" id="myTopnav">
-        <a href="<?php echo __SITE_URL . '/index.php?rt=home'; ?>" class="active" id="home">Home</a>
+        <a href="<?php echo __SITE_URL . '/index.php?rt=users/home'; ?>" class="active" id="home">Home</a>
         <a href="<?php echo __SITE_URL . '/index.php?rt=transactions'; ?>" id="transactions">Transactions</a>
         <a href="<?php echo __SITE_URL . '/index.php?rt=categories'; ?>" id="categories">Categories</a>
         <a href="<?php echo __SITE_URL . '/index.php?rt=savings'; ?>" id="savings">Savings</a>
-        <a href="<?php echo __SITE_URL . '/index.php?rt=report'; ?>" id="report">Report</a>
-        <a href="<?php echo __SITE_URL . '/index.php?rt=logout'; ?>" id="logout">Log out</a>
+        <a href="<?php echo __SITE_URL . '/index.php?rt=reports'; ?>" id="report">Report</a>
+        <a href="<?php echo __SITE_URL . '/index.php?rt=users/logout'; ?>" id="logout">Log out</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
         </a>
