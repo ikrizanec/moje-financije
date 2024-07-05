52b065dc010ddc5c959970c816b0ae80373a4609
hodziana <164337649+hodziana@users.noreply.github.com>
2024-07-05 19:14:13 +0200

    Update TransactionsService

--- model/transactions_service.class.php
+++ model/transactions_service.class.php
@@ -5,13 +5,32 @@ require_once __SITE_PATH .  '/model/transactions.class.php';
 
 class TransactionsService
 {
-    function getTransactionsByType( $type )
+    function getAllTransactions( $id_user )
+    {
+        try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT * FROM transactions WHERE id_user=:id_user' );
+			$st->execute( array( 'id_user' => $id_user ) );
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+        $arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Transactions ($row['id_transactions'], $row['id_user'], $row['id_category'], $row['amount'],
+                    $row['transaction_date'], $row['description'], $row['type'] );
+		}
+		return $arr;
+    }
+
+    function getTransactionsByType( $id_user, $type )
 	{
 		try
 		{
 			$db = DB::getConnection();
-			$st = $db->prepare( 'SELECT * FROM transactions WHERE type=:type' );
-			$st->execute( array( 'type' => $type ) );
+			$st = $db->prepare( 'SELECT * FROM transactions WHERE id_user=:id_user AND type=:type' );
+			$st->execute( array( 'id_user' => $id_user, 'type' => $type ) );
 		}
 		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
 
@@ -19,9 +38,41 @@ class TransactionsService
 		while( $row = $st->fetch() )
 		{
 			$arr[] = new Transactions ($row['id_transactions'], $row['id_user'], $row['id_category'], $row['amount'],
-            $row['transaction_date'], $row['description'], $row['type'] );
+                    $row['transaction_date'], $row['description'], $row['type'] );
 		}
 		return $arr;
 	}
+
+    function getTransactionsByDate( $id_user, $begin_date, $end_date )
+    {
+        try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT * FROM transactions WHERE id_user=:id_user AND transaction_date>=:begin_date AND transaction_date<=:end_date' );
+			$st->execute( array( 'id_user' => $id_user, 'begin_date' => $begin_date, 'end_date' => $end_date ) );
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Transactions ($row['id_transactions'], $row['id_user'], $row['id_category'], $row['amount'],
+                    $row['transaction_date'], $row['description'], $row['type'] );
+		}
+		return $arr;
+    }
+
+    function addTransaction( $id_user, $id_category, $amount, $transaction_date, $description, $type )
+    {
+        try
+        {
+            $db = DB::getConnection();
+            $st = $db->prepare('INSERT INTO transactions( id_user, id_category, amount, transaction_date, description, type ) VALUES
+					  (:id_user, :id_category, :amount, :transaction_date, :description, :type )');
+            $st->execute( array( 'id_user' => $id_user , 'id_category' => $id_category, 'amount' => $amount, 'transaction_date' => $transaction_date,
+            'description' => $description, 'type' => $type ) );
+        }
+        catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+    }
 };
 ?>
