b761bef16ce0ce7f97828ab88c4eae75bd5439fa
hodziana <164337649+hodziana@users.noreply.github.com>
2024-07-05 19:47:21 +0200

    Update SavingService

--- model/saving_service.class.php
+++ model/saving_service.class.php
@@ -10,7 +10,7 @@ class SavingService
 		try
 		{
 			$db = DB::getConnection();
-			$st = $db->prepare( 'SELECT * FROM saving WHERE id_user=:id_user' );
+			$st = $db->prepare( 'SELECT * FROM savings WHERE id_user=:id_user' );
 			$st->execute( array( 'id_user' => $id_user ) );
 		}
 		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
