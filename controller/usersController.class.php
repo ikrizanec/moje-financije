680b1b44b882378eb556d05e65f89ae96e2e1700
Ivana Križanec <ivanakrizanec@hotmail.com>
2024-07-06 14:40:44 +0200

    dodan login

--- controller/usersController.class.php
+++ controller/usersController.class.php
@@ -1,13 +1,59 @@
 <?php
+
+require_once __SITE_PATH .  '/model/user_service.class.php';
+
 class UsersController {
 
     public function index() {
-        header('Location: ' . __SITE_URL . '/index.php?rt=users/login');
+        var_dump($_POST);
+        include __SITE_PATH . '/view/login.php';
     }
 
     public function login() {
-        include __SITE_PATH . '/view/login.php';
-        //dodat logiku za logiranje (ako prode, header location transactions list?..., ako ne  $error, pa header login form)
+        
+        
+        echo "pocinje prijava/n";
+        //var_dump($_POST['username']);
+        echo "/n";
+        //var_dump($_POST['password']);echo "\n post:";
+        var_dump($_POST);
+
+		if( !isset( $_POST['username'] ) || !isset( $_POST['password'] ) )
+		{
+			include __SITE_PATH . '/view/login.php';
+		}
+		else {
+            echo "else\n";
+            $us = new UserService();
+            $user = $us->getUserByUsername( $_POST['username'] );
+            echo "dohvacenuser\n";
+            var_dump($_POST['username']);
+
+            if( $user === null )
+            {
+                echo "User wih this username does not exist.\n";
+                header( 'Location: ' . __SITE_URL . '/index.php?rt=users/login' );
+            }
+            else if( !password_verify( $_POST['password'], $user->password_hash ) )
+            {
+                echo "Incorrent password.\n";
+                header( 'Location: ' . __SITE_URL . '/index.php?rt=users/login' );
+            }
+            else
+            {
+                echo "Login successful.\n";
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['id_user'] = $user -> id_user;
+                $_SESSION['name'] = $user -> name;
+                $_SESSION['surname'] = $user -> surname;
+                $_SESSION['admin'] = $user -> is_admin;
+
+                header( 'Location: ' . __SITE_URL . '/index.php?rt=users/home' );
+            }
+
+
+            //header( 'Location: ' . __SITE_URL . '/index.php?rt=users/home' );
+		}
     }
 
     private function showLoginForm($error = null) {
--- view/login.php
+++ view/login.php
@@ -4,23 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Moje financije</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo __SITE_URL . '/css/login.css';?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo __SITE_URL . '/style/login.css';?>">
 </head>
 <body>
-	<form action="<?php echo __SITE_URL . '/index.php?rt=users/login'; ?>" method="post">
+	<form method="post" action="<?php echo __SITE_URL . '/index.php?rt=users/login'; ?>" >
 		<div class="login-box">
 			<h1>Login</h1>
 
 			<div class="textbox">
 				<i class="fa fa-user" aria-hidden="true"></i>
-				<input type="text" placeholder="username"
-						name="username" value="">
+				<input type="text" placeholder="username" name="username" id="username">
 			</div>
 
 			<div class="textbox">
 				<i class="fa fa-lock" aria-hidden="true"></i>
-				<input type="password" placeholder="password"
-						name="password" value="">
+				<input type="password" placeholder="password" name="password" id="password">
 			</div>
 
 			<input class="button" type="submit" name="login" value="sign in">
