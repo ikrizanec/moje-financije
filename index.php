68ce46dfc7cadb1fcdbcafe00c780f483d904eb9
Ivana K <ivanakristic3@hotmail.com>
2024-07-06 11:11:41 +0000

    Ispravljena greška u generiranju site_url-a

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 session_start();
 
 define('__SITE_PATH', realpath(dirname(__FILE__)));
-define('__SITE_URL', dirname($_SERVER['PHP_SELF']));
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$site_url = $protocol . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+define('__SITE_URL', $site_url);
 
 if (!isset($_GET['rt'])) {
     $con = 'users';
