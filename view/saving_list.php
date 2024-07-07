9b3192a75d6ce1addcdf22d1e586273b3a53846f
hodziana <164337649+hodziana@users.noreply.github.com>
2024-07-07 20:35:32 +0200

    promjene za testiranje ajaxa iz savingsContoller-a

--- view/saving_list.php
+++ view/saving_list.php
@@ -1,8 +1,40 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
-<?php
-// stednje ce se dobit preko varijable $savings
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moje financije</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <main>
+    <p id="test"></p>
 
-?>
+    <script>
+        $.ajax(
+        {
+            url: '<?php echo __SITE_URL; ?>/index.php?rt=savings',
+            type: 'GET',
+            data: 
+            { 
+                action: "list"
+            },
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                let output = "";
+                data.savings.forEach(saving => {
+                    output += `<p> Name: ${saving.savings_name}, Description: ${saving.savings_goal}</p>`;
+                });
+                $('#test').html(output);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('Error:', textStatus, errorThrown);
+                $('#test').html('An error occurred while fetching savings.');
+            }
+        } );
+    </script>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
