9188c4f9ef9cdcc82286c8bc9d48e0212dda1f42
hodziana <164337649+hodziana@users.noreply.github.com>
2024-07-07 20:31:48 +0200

    funkcija index pomocu jqueryja

--- controller/savingsController.class.php
+++ controller/savingsController.class.php
@@ -1,18 +1,26 @@
 <?php
 require_once __SITE_PATH . '/model/saving_service.class.php';
+require_once __SITE_PATH . '/model/saving.class.php';
 require_once __SITE_PATH . '/model/users_service.class.php';
 
 class SavingsController {
 
     public function index() {
-        //dodat dohvacanje svih transakcija u varijablu $savings
-        if ( isset($_SESSION['username'] ) ) {
-            $us = new UserService();
-            $user = $us->getUserByUsername( $_POST['username'] );
-            $id_user = $user->id_user;
-            $ss = new SavingService();
-            $savings = $ss->getSavingsByUserID( $id_user );
-            
+        if ( isset( $_GET['action'] ) && isset( $_SESSION['username'] ) && $_GET['action'] === 'list') {
+            try {
+                $us = new UserService();
+                $user = $us->getUserByUsername( $_SESSION['username'] );
+                $id_user = $user->id_user;
+                $ss = new SavingService();
+                $savings = $ss->getSavingsByUserID( $id_user );
+            } catch (PDOException $e) {
+                error_log("PDO Error: " . $e->getMessage());
+                $this->sendJSONandExit(['status' => 'error', 'message' => 'Database error occurred']);
+            }
+
+            $message['savings'] = $savings;
+            $this->sendJSONandExit($message);
+        } else {
             include __SITE_PATH . '/view/saving_list.php';
         }
     }
@@ -20,32 +28,12 @@ class SavingsController {
     public function add() {
         //ako su poslani podaci s forme, dodat ih, prihazat poruku o uspješnom dodavanju ($message) i vratit se na formu za dodavanje:
         $message = '';
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if ( isset($_SESSION['username']) && isset($_POST['savings_name']) && isset($_POST['savings_goal'] && isset($_POST['deadline'])) 
-            && !empty($_POST['savings_name']) && !empty($_POST['savings_goal'])  && !empty($_POST['deadline']) ) {
-                $ss = new SavingService();
-                $savingsName = $_POST['savings_name'];
-                $savingsGoal = $_POST['savings_goal'];
-                $savingsDeadline = $_POST['deadline'];
-
-                $us = new UserService();
-                $user = $us->getUserByUsername( $_POST['username'] );
-                $idUser = $user->id_user;
-                $balance = 0;
-
-                $ss->addSaving( $idUser, $savingsName, $savingsGoal, $balance, $savingsDeadline );
-                $message = "Saving successfully created.";
-            } else {
-                $message = "Please fill in savings name, goal and deadline.";
-            }
-        }
         include __SITE_PATH . '/view/saving_add.php';
     }
 
     public function add_contribution() {
         //ako su poslani podaci s forme, dodat ih, prihazat poruku o uspješnom dodavanju ($message) i vratit se na formu za dodavanje:
-        $message = null;
+        $message = '';
         include __SITE_PATH . '/view/saving_contribution_add.php';
     }
 
