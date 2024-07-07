d2c259853f4cccf75c26bdf8ff3984a5f44aa80b
duvnjste <stefani.duvnjak98@gmail.com>
2024-07-07 14:39:38 +0200

    dodan footer u view-ove i style

--- view/_footer.php
+++ view/_footer.php
@@ -1,2 +1,8 @@
+
+</main>
+<footer>
+    Authors:<br>
+    Ana Hodžić &middot; Ivana Kristić &middot; Ivana Križanec &middot; Stefani Duvnjak
+</footer>
 </body>
 </html>
--- view/user_home.php
+++ view/user_home.php
@@ -3,3 +3,5 @@
 <?php
 // transakcije ce se dobit preko varijable $transactions
 ?>
+
+<?php require_once __SITE_PATH . '/view/_footer.php'; ?>
