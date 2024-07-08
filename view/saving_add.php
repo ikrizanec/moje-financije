9a2511d004b5b7a8b87972ee34be93724a70f737
duvnjste <stefani.duvnjak98@gmail.com>
2024-07-09 01:37:57 +0200

    dodana pocetna logika

--- view/saving_add.php
+++ view/saving_add.php
@@ -1,8 +1,67 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
-<?php
-//u varijabli $message poruka
-?>
-<form action="<?php echo __SITE_URL . '/index.php?rt=savings/add'; ?>" method="post">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Saving</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <h1 class="title">add new saving</h1>
+
+    <form id="addSavingsForm" class="form">
+        <div>
+            <label for="name" class="label">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div>
+            <label for="name" class="label">Goal:</label>
+            <input type="text" id="goal" name="goal" required>
+        </div>
+        <div>
+            <label for="name" class="label">Deadline:</label>
+            <input type="text" id="deadline" name="deadline" required>
+        </div>
+        <div class="buttonContainer">
+            <button type="submit" class="button">create</button>
+        </div>
+    </form>
+
+    <div id="successMessage"></div>
+
+    <script>
+        $(document).ready(function() {
+            $('#addSavingsForm').submit(function(event) {
+                event.preventDefault();
+
+                $.ajax({
+                    url: '<?php echo __SITE_URL; ?>/index.php?rt=savings/add',
+                    type: 'POST',
+                    data: 
+                    { 
+                        action: "add",
+                        name: $('#name').val(),
+                        goal: $('#goal').val(),
+                        deadline: $('#deadline').val(),
+                    },
+                    dataType: 'json',
+                    success: function(data) {
+                        $('#successMessage').text(data.message);
+                        // Clear form fields
+                        $('#name').val('');
+                        $('#description').val('');
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.error('Error:', textStatus, errorThrown);
+                        $('#successMessage').text('Failed to add saving.');
+                    }
+                });
+            });
+        });
+    </script>
+</body>
+</html>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
--- view/saving_list.php
+++ view/saving_list.php
@@ -1,16 +1,12 @@
+
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moje financije</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
 <body>
-    <main>
-    <p id="test"></p>
+    <div class="buttonContainer">
+        <button id="addSavingsBtn" class="button">add new savings</button>
+    </div>
+    <p id="test" class="test"></p>
+
 
     <script>
         $.ajax(
@@ -23,17 +19,49 @@
             },
             dataType: 'json',
             success: function(data) {
-                let output = "";
-                data.savings.forEach(saving => {
-                    output += `<p> Name: ${saving.savings_name}, Goal: ${saving.savings_goal}, Balance: ${saving.current_balance}, Deadline: ${saving.deadline}</p>`;
+                let output = `<table id="savingsTable">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Goal</th>
+                                <th>Balance</th>
+                                <th>Deadline</th>
+                                <th>Contribute</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+                data.savings.forEach(savings => {
+                    output += `<tr>
+                        <td>${savings.savings_name}</td>
+                        <td>${savings.savings_goal}</td>
+                        <td>${savings.current_balance}</td>
+                        <td>${savings.deadline}</td>
+                        <td><button id="addContributeBtn" class="button" value="${savings.id_savings}">contribute</button></td>
+                       </tr>`;
                 });
-                $('#test').html(output);
+                output += `</tbody></table>`;
+                $('.test').html(output);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.error('Error:', textStatus, errorThrown);
-                $('#test').html('An error occurred while fetching savings.');
+                $('.test').html('An error occurred while fetching categories.');
             }
         } );
+
+        $('#addSavingsBtn').click(function() {
+            window.location.href = '<?php echo __SITE_URL; ?>/index.php?rt=savings/add';
+        });
+
+        $('#addContributeBtn').click(function() {
+            let id_savings = $(this).data('id_savings');
+            let id_user = '<?php echo $_SESSION['id_user']; ?>';
+
+            
+            window.location.href = '<?php echo __SITE_URL; ?>/index.php?rt=savings/add&id_savings=' + id_savings + '&id_user=' + id_user;
+        });
+
     </script>
+</body>
+</html>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
