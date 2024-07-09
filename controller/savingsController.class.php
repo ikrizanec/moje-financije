52e9a85647f9c2bba831c6a4967fee635b4a3186
hodziana <164337649+hodziana@users.noreply.github.com>
2024-07-10 00:33:16 +0200

    gotov savingsController

--- controller/savingsController.class.php
+++ controller/savingsController.class.php
@@ -25,25 +25,34 @@ class SavingsController {
         }
     }
 
-    public function add() {
-        if ( isset( $_POST['action'] ) && isset( $_SESSION['username'] ) && $_POST['action'] === 'add' )
+    public function add_contribution() {
+        if ( isset( $_POST['action'] ) && isset( $_SESSION['username'] ) && $_POST['action'] === 'add_contribution' )
         {
             $us = new UserService();
             $user = $us->getUserByUsername( $_SESSION['username'] );
             $id_user = $user->id_user;
-            $ss = new SavingService();
-            $savings_name = $_POST['savings_name'];
-            $savings_goal = $_POST['savings_goal'];
-            $current_balance = 0;
-            $deadline = $_POST['deadline'];
-            $ss->addSaving( $id_user, $savings_name, $savings_goal, $current_balance, $deadline );
-            
-            $message['message'] = 'Saving added successfully!';
-            $this->sendJSONandExit($message);
+            $sc = new SavingsContributionsService();
+            $id_savings = $_POST['id_savings'];
+            $payment_amount = (int)$_POST['payment_amount'];
+            $contribution_date = date('Y-m-d');
+            $new_balance = $user->balance - $payment_amount;
+            if ( $new_balance >= 0 )
+            {
+                $sc->addSavingsContributions( $id_savings, $payment_amount, $contribution_date );
+                $us->updateBalance( $id_user, $new_balance );
+                $ss = new SavingService();
+                $ss->updateSavingsBalance( $id_savings, $payment_amount );
+                $message['message'] = 'Saving added successfully!';
+                $this->sendJSONandExit($message);
+            }
+            else {
+                $message['message'] = 'Balance too low to make this contribution.';
+                $this->sendJSONandExit($message);
+            }
         } 
         else 
         {
-            include __SITE_PATH . '/view/saving_add.php';
+            include __SITE_PATH . '/view/saving_list.php.php';
         }
     }
 
@@ -60,27 +69,7 @@ class SavingsController {
             $new_balance = $user->balance - $payment_amount;
             if ( $new_balance >= 0 )
             {
-                $sc->addSavingsContributions( $id_savings, $payment_amount, $contribution_date );
-                $message['message'] = 'Saving added successfully!';
-                $us->updateBalance( $id_user, $new_balance );
-                $ss = new SavingService();
-                $saving = $ss->getSavingById( $id_savings );
-                $current_balance = $saving->current_balance;
-                $new_savings_balance = $current_balance + $payment_amount;
-                $ss->updateSavingsBalance( $id_savings, $new_savings_balance );
-
-                $this->sendJSONandExit($message);
-            }
-            else {
-                $message['message'] = 'Balance too low to make this contribution.';
-                $this->sendJSONandExit($message);
-            }
-        } 
-        else 
-        {
-            include __SITE_PATH . '/view/saving_list.php.php';
-        }
-    }
+          
     public function sendJSONandExit( $message )
     {
         header( 'Content-type:application/json;charset=utf-8' );
